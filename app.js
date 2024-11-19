f51345b7746ed70f9c83dda0eb5ec68ec08da347
“tabihsfaisal“ <tabish.faisaltn@gmail.com>
2024-11-19 16:37:08 +0100

    il primo test fatto

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const { log } = require('console');
 const express = require('express');
 const app = express();
 const port = 3000;
+
+
+app.get('/posts',(req,res)=>{
+    res.send('Lista dei post');
+})
+
+app.listen(port,()=>{
+    console.log('lista dei post');
+    
+})
